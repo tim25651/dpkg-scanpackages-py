d233b1d76a7ddfa8a0625f663b94eabc8ac9e990
Tim Hörmann <t.hoermann@audivir.de>
2024-11-03 23:13:35 +0100

    Added type hints, removed camelCase, small QOL updates

--- dpkg-scanpackages.py
+++ dpkg-scanpackages.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+"""Scan packages in a directory and generate a Packages file."""
 # Copyright 2018 Raymond Velasquez
 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,159 +14,251 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Script: dpkg-scanpackages.py
+# Script: dpkg-scanpackages.py # noqa: ERA001
 # Author: Raymond Velasquez <at.supermamon@gmail.com>
+from __future__ import annotations
 
-script_name    = 'dpkg-scanpackages.py'
-script_version = '0.4.1'
+import argparse
+import sys
+from contextlib import contextmanager
+from os import PathLike
+from pathlib import Path
+from typing import TYPE_CHECKING, TypeAlias
 
-import glob, sys, os, argparse
 from pydpkg import Dpkg
+from typing_extensions import override
+
+if TYPE_CHECKING:
+    from collections.abc import Generator
+
+    from _typeshed import SupportsWrite
+
+StrPath: TypeAlias = str | PathLike
+
+
+script_name = "dpkg-scanpackages.py"
+script_version = "0.4.1"
+
 
 class DpkgInfo:
-    def __init__(self,binary_path):
+    """Information about a dpkg package."""
+
+    def __init__(self, binary_path: StrPath) -> None:
+        """Initialize the class."""
         self.binary_path = binary_path
-        self.headers = {}
+        self.headers: dict[str, str] = {}
         pkg = Dpkg(self.binary_path)
 
         # build the information for the apt repo
         self.headers = pkg.headers
-        self.headers['Filename'] = pkg.filename.replace("\\",'/')
-        self.headers['Size'] = pkg.filesize
-        self.headers['MD5sum'] = pkg.md5
-        self.headers['SHA1'] = pkg.sha1
-        self.headers['SHA256'] = pkg.sha256
+        self.headers["Filename"] = pkg.filename.replace("\\", "/")
+        self.headers["Size"] = pkg.filesize
+        self.headers["MD5sum"] = pkg.md5
+        self.headers["SHA1"] = pkg.sha1
+        self.headers["SHA256"] = pkg.sha256
 
-    def __str__(self):
-        pretty = ''
-        keyOrder=[
-            'Package','Version','Architecture','Maintainer',
-            'Depends','Conflicts','Breaks','Replaces',
-            'Filename','Size',
-            'MD5sum','SHA1','SHA256',
-            'Section','Description'
+    @override
+    def __str__(self) -> str:
+        pretty = ""
+        key_order = [
+            "Package",
+            "Version",
+            "Architecture",
+            "Maintainer",
+            "Depends",
+            "Conflicts",
+            "Breaks",
+            "Replaces",
+            "Filename",
+            "Size",
+            "MD5sum",
+            "SHA1",
+            "SHA256",
+            "Section",
+            "Description",
         ]
         # add as per key order
-        for key in keyOrder:
+        for key in key_order:
             if key in self.headers:
-                pretty = pretty + ('{0}: {1}\n').format(key,self.headers[key])
+                pretty = pretty + (f"{key}: {self.headers[key]}\n")
 
         # add the rest alphabetically
         for key in sorted(self.headers.keys()):
-            if not key in keyOrder:
-                pretty = pretty + ('{0}: {1}\n').format(key,self.headers[key])
+            if key not in key_order:
+                pretty = pretty + (f"{key}: {self.headers[key]}\n")
         return pretty
 
+
 class DpkgScanpackages:
-    def __init__(self,binary_path, multiversion=None,packageType=None,arch=None,output=None):
-        self.binary_path = binary_path
+    """Scan packages in a directory and generate a Packages file."""
+
+    def __init__(
+        self,
+        binary_path: StrPath,
+        multiversion: bool | None = None,
+        package_type: str | None = None,
+        arch: str | None = None,
+        output: StrPath | None = None,
+    ) -> None:
+        """Initialize the class."""
+        self.binary_path = Path(binary_path)
 
         # throw an error if it's an invalid path
-        if not os.path.isdir(self.binary_path):
-            raise ValueError('binary path {0} not found'.format(self.binary_path))
+        if not self.binary_path.is_dir():
+            raise ValueError(f"binary path {self.binary_path} not found")
 
         # options
         self.multiversion = multiversion if multiversion is not None else False
-        self.packageType = packageType if packageType is not None else 'deb'
+        self.package_type = package_type if package_type is not None else "deb"
         self.arch = arch
-        self.output = output
+        self.output = Path(output) if output is not None else None
 
-        self.packageList = []
+        self.package_list: list[DpkgInfo] = []
 
-    def __get_packages(self):
+    def _get_packages(self) -> None:
+        """Get the packages."""
         # get all files
-        files = glob.glob( os.path.join(self.binary_path,"") + "*" + self.packageType )
+        files = self.binary_path.glob(f"*{self.package_type}")
+
         for fname in files:
             # extract the package information
             pkg_info = DpkgInfo(fname)
 
             # if arch is defined and does not match package, move on to the next
-            if self.arch is not None:
-                if str(pkg_info.headers['Architecture']) != self.arch:
-                    continue
+            if (
+                self.arch is not None
+                and str(pkg_info.headers["Architecture"]) != self.arch
+            ):
+                continue
 
             # if --multiversion switch is passed, append to the list
-            if self.multiversion==True:
-                self.packageList.append(pkg_info)
+            if self.multiversion:
+                self.package_list.append(pkg_info)
             else:
                 # finf if package is already in the list
-                matchedItems = [(index,pkg) for (index,pkg) in enumerate(self.packageList) if self.packageList and pkg.headers['Package'] == pkg_info.headers['Package']]
-                if len(matchedItems)==0:
+                matched_items = [
+                    (index, pkg)
+                    for (index, pkg) in enumerate(self.package_list)
+                    if self.package_list
+                    and pkg.headers["Package"] == pkg_info.headers["Package"]
+                ]
+                if len(matched_items) == 0:
                     # add if not
-                    self.packageList.append(pkg_info)
+                    self.package_list.append(pkg_info)
                 else:
                     # compare versions and add if newer
-                    matchedIndex = matchedItems[0][0]
-                    matchedItem = matchedItems[0][1]
+                    matched_index = matched_items[0][0]
+                    matched_item = matched_items[0][1]
 
-                    dpkg = Dpkg(pkg_info.headers['Filename'])
-                    if dpkg.compare_version_with(matchedItem.headers['Version']) == 1:
-                        self.packageList[matchedIndex] = pkg_info
+                    dpkg = Dpkg(pkg_info.headers["Filename"])
+                    if dpkg.compare_version_with(matched_item.headers["Version"]) == 1:
+                        self.package_list[matched_index] = pkg_info
 
-    def scan(self,returnList=False):
-        self.__get_packages()
-        if (returnList):
-            return (self.packageList)
-        else:
-            if not self.output is None:
-                file = open(self.output, "wb")
+    def scan(self, return_list: bool = False) -> list[DpkgInfo] | None:
+        """Scan the packages."""
+        self._get_packages()
+        if return_list:
+            return self.package_list
 
-            for p in self.packageList:
-                if self.output is None:
-                    print(str(p))
-                else:
-                    file.write(bytes(str(p),'utf-8'))
-                    file.write(bytes("\n",'utf-8'))
-            if not self.output is None:
-                file.close()
+        @contextmanager
+        def smart_open() -> Generator[SupportsWrite[bytes]]:
+            """Open the output file or stdout."""
+            if self.output is None:
+                yield sys.stdout.buffer
+            else:
+                with self.output.open("wb") as file:
+                    yield file
+
+        with smart_open() as file:
+            for p in self.package_list:
+                p_b = str(p).encode("utf-8")
+                file.write(p_b + b"\n")
+        return None
 
 
-def print_error(msg):
-    COLOR_RED   = '\033[91m'
-    COLOR_RESET = "\033[0;0m"
-    COLOR_BOLD  = '\033[1m'
-    print('{0}:{1}{2} error{3}: {4}'.format(script_name,COLOR_RED,COLOR_BOLD,COLOR_RESET,msg))
-    print('')
-    print('Use --help for program usage information.')
+def print_error(err: ValueError) -> None:
+    """Print an error message."""
+    import termcolor
 
-def main():
+    error_msg = termcolor.colored("error", "red", attrs=["bold"])
+    print(f"{script_name}: {error_msg}: {err}")  # noqa: T201
+    print()  # noqa: T201
+    print("Use --help for program usage information.")  # noqa: T201
+
+
+class ScanPackagesNamespace(argparse.Namespace):
+    """Namespace for command-line arguments."""
+
+    binary_path: Path
+    multiversion: bool
+    arch: str | None
+    type: str
+    output: Path | None
+
+
+def parse_args() -> ScanPackagesNamespace:
+    """Parse command-line arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--version',
-                        action='version',
-                        version='Debian %(prog)s version '+script_version+'.',
-                        help='show the version.')
-    parser.add_argument('-m','--multiversion',
-                        default=False,
-                        action="store_true",
-                        dest='multiversion',
-                        help='allow multiple versions of a single package.')
-    parser.add_argument('-a','--arch',
-                        default=None,
-                        action="store",
-                        dest='arch',
-                        help='architecture to scan for.')
-    parser.add_argument('-t','--type',
-                        default='deb',
-                        action="store",
-                        dest='type',
-                        help='scan for <type> packages (default is \'deb\').')
-    parser.add_argument('-o','--output',
-                        action="store",
-                        dest='output',
-                        help='Write to file instead of stdout')
-    parser.add_argument('binary_path')
+    parser.add_argument(
+        "-v",
+        "--version",
+        action="version",
+        version="Debian %(prog)s version " + script_version + ".",
+        help="show the version.",
+    )
+    parser.add_argument(
+        "-m",
+        "--multiversion",
+        default=False,
+        action="store_true",
+        dest="multiversion",
+        help="allow multiple versions of a single package.",
+    )
+    parser.add_argument(
+        "-a",
+        "--arch",
+        type=str,
+        default=None,
+        action="store",
+        dest="arch",
+        help="architecture to scan for.",
+    )
+    parser.add_argument(
+        "-t",
+        "--type",
+        type=str,
+        default="deb",
+        action="store",
+        dest="type",
+        help="scan for <type> packages (default is 'deb').",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        type=Path,
+        action="store",
+        dest="output",
+        help="Write to file instead of stdout",
+    )
+    parser.add_argument("binary_path", type=Path, help="path to the binary directory")
 
-    args = parser.parse_args()
+    return parser.parse_args()  # type: ignore[return-value]
+
+
+def main() -> None:
+    """Main entry point."""
+    args = parse_args()
     try:
         DpkgScanpackages(
             binary_path=args.binary_path,
             multiversion=args.multiversion,
             arch=args.arch,
-            packageType=args.type,
-            output=args.output
+            package_type=args.type,
+            output=args.output,
         ).scan()
     except ValueError as err:
-        print_error(err.message)
+        print_error(err)
+
 
 if __name__ == "__main__":
     main()
